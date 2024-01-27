2bdf319424fdaa407016856bdd88a4f03475fef5
Scott Bedard <scottbedard90@gmail.com>
2024-01-27 16:42:38 -0600

    return json of parsed board

--- src/commands/parse.rs
+++ src/commands/parse.rs
@@ -1,12 +1,8 @@
 use crate::game::hexchess::Hexchess;
 
 pub fn execute(fen: String) -> Result<String, String> {
-    let hexchess = match Hexchess::from(fen.as_str()) {
-        Ok(result) => result,
-        Err(failure) => return Err(failure.to_string()),
-    };
-
-    panic!("hexchess: {:?}", hexchess)
-
-//     Err("Not implemented")
+    match Hexchess::from(fen.as_str()) {
+        Ok(result) => Ok(result.to_json()),
+        Err(failure) => Err(failure.to_string()),
+    }
 }
