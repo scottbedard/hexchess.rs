cc05fe4b305eb22a1d95844eda9e5f729a5c1b1d
Scott Bedard <scottbedard90@gmail.com>
2024-07-21 09:22:51 -0500

    file

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -5,8 +5,9 @@ import path from 'path'
 function run() {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
+  const file = fs.readFileSync(path.resolve(__dirname, '../pkg/package.json'))
   const base = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'))
-  const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../pkg/package.json'), 'utf-8'))
+  const pkg = JSON.parse(file, 'utf-8')
 
   pkg.main = 'hexchess.js'
   pkg.name = '@bedard/hexchess'
