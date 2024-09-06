7e82034c7778a35b1c85da2d215057495a56212c
Scott Bedard <scottbedard90@gmail.com>
2024-09-05 23:09:26 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -79,29 +79,67 @@ mod tests {
 
     #[test]
     fn test_apply_sequence() {
-        let app = App {
+        let app1 = App {
             command: Command::ApplySequence {
                 fen: "b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1".to_string(),
                 sequence: "g4g5 e7e6".to_string(),
             }
         };
 
-        let output = main_body(app);
+        let app2 = App {
+            command: Command::ApplySequence {
+                fen: "whoops".to_string(),
+                sequence: "g4g5 e7e6".to_string(),
+            }
+        };
 
-        assert_eq!(output, Ok("b/qbk/n1b1n/r5r/ppp1ppppp/4p6/5PP4/4P6/3P1B1P3/2P2B2P2/1PRNQBKNRP1 w - 0 2".to_string()));
+        let app3 = App {
+            command: Command::ApplySequence {
+                fen: "b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1".to_string(),
+                sequence: "whoops".to_string(),
+            }
+        };
+
+        let output1 = main_body(app1);
+        let output2 = main_body(app2);
+        let output3 = main_body(app3);
+
+        assert_eq!(output1, Ok("b/qbk/n1b1n/r5r/ppp1ppppp/4p6/5PP4/4P6/3P1B1P3/2P2B2P2/1PRNQBKNRP1 w - 0 2".to_string()));
+        assert_eq!(output2, Err("invalid_board".to_string()));
+        assert_eq!(output3, Err("Invalid notation at index 0: whoops".to_string()));
     }
 
     #[test]
     fn test_get_targets() {
-        let app = App {
+        let app1 = App {
             command: Command::GetTargets {
                 fen: "b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1".to_string(),
                 position: "g4".to_string(),
             }
         };
 
-        let output = main_body(app);
+        let output1 = main_body(app1);
 
-        assert_eq!(output, Ok("g4g5,g4g6".to_string()));
+        let app2 = App {
+            command: Command::GetTargets {
+                fen: "b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1".to_string(),
+                position: "whoops".to_string(),
+            }
+        };
+
+        let output2 = main_body(app2);
+
+        let app3 = App {
+            command: Command::GetTargets {
+                fen: "whoops".to_string(),
+                position: "g4".to_string(),
+            }
+        };
+
+        let output3 = main_body(app3);
+
+        assert_eq!(output1, Ok("g4g5,g4g6".to_string()));
+        assert_eq!(output2, Err("invalid_position".to_string()));
+        assert_eq!(output3, Err("invalid_board".to_string()));
     }
 }
