d9ad073bd45676952ce8e20662ae2feeecbf7c49
Scott Bedard <scottbedard90@gmail.com>
2025-04-01 21:31:14 -0500

    walk

--- src/hexchess/utils.rs
+++ src/hexchess/utils.rs
@@ -1,5 +1,29 @@
 use crate::hex;
 
+use crate::constants::{
+    Color, Piece, GRAPH
+};
+
+use crate::hexchess::hexchess::Hexchess;
+
+/// get the color of a piece
+pub fn get_color(piece: Piece) -> Color {
+    match piece {
+        Piece::WhitePawn => Color::White,
+        Piece::WhiteKnight => Color::White,
+        Piece::WhiteBishop => Color::White,
+        Piece::WhiteRook => Color::White,
+        Piece::WhiteQueen => Color::White,
+        Piece::WhiteKing => Color::White,
+        Piece::BlackPawn => Color::Black,
+        Piece::BlackKnight => Color::Black,
+        Piece::BlackBishop => Color::Black,
+        Piece::BlackRook => Color::Black,
+        Piece::BlackQueen => Color::Black,
+        Piece::BlackKing => Color::Black,
+    }
+}
+
 /// test if position is black en passant target
 pub fn is_legal_black_en_passant(position: &u8) -> bool {
     match position {
@@ -173,5 +197,178 @@ pub fn to_index(source: &str) -> Result<u8, ()> {
         "k1" => Ok(89),
         "l1" => Ok(90),
         _ => Err(()),
+    }
+}
+
+pub fn to_position(index: &u8) -> &'static str {
+    match index {
+        0 => "f11",
+        1 => "e10",
+        2 => "f10",
+        3 => "g10",
+        4 => "d9",
+        5 => "e9",
+        6 => "f9",
+        7 => "g9",
+        8 => "h9",
+        9 => "c8",
+        10 => "d8",
+        11 => "e8",
+        12 => "f8",
+        13 => "g8",
+        14 => "h8",
+        15 => "i8",
+        16 => "b7",
+        17 => "c7",
+        18 => "d7",
+        19 => "e7",
+        20 => "f7",
+        21 => "g7",
+        22 => "h7",
+        23 => "i7",
+        24 => "k7",
+        25 => "a6",
+        26 => "b6",
+        27 => "c6",
+        28 => "d6",
+        29 => "e6",
+        30 => "f6",
+        31 => "g6",
+        32 => "h6",
+        33 => "i6",
+        34 => "k6",
+        35 => "l6",
+        36 => "a5",
+        37 => "b5",
+        38 => "c5",
+        39 => "d5",
+        40 => "e5",
+        41 => "f5",
+        42 => "g5",
+        43 => "h5",
+        44 => "i5",
+        45 => "k5",
+        46 => "l5",
+        47 => "a4",
+        48 => "b4",
+        49 => "c4",
+        50 => "d4",
+        51 => "e4",
+        52 => "f4",
+        53 => "g4",
+        54 => "h4",
+        55 => "i4",
+        56 => "k4",
+        57 => "l4",
+        58 => "a3",
+        59 => "b3",
+        60 => "c3",
+        61 => "d3",
+        62 => "e3",
+        63 => "f3",
+        64 => "g3",
+        65 => "h3",
+        66 => "i3",
+        67 => "k3",
+        68 => "l3",
+        69 => "a2",
+        70 => "b2",
+        71 => "c2",
+        72 => "d2",
+        73 => "e2",
+        74 => "f2",
+        75 => "g2",
+        76 => "h2",
+        77 => "i2",
+        78 => "k2",
+        79 => "l2",
+        80 => "a1",
+        81 => "b1",
+        82 => "c1",
+        83 => "d1",
+        84 => "e1",
+        85 => "f1",
+        86 => "g1",
+        87 => "h1",
+        88 => "i1",
+        89 => "k1",
+        90 => "l1",
+        _ => panic!("invalid position index: {}", index),
+    }
+}
+
+/// walk along the board in a given direction
+pub fn walk(hexchess: &Hexchess, from: u8, direction: u8, color: Color) -> Vec<u8> {
+    let mut path: Vec<u8> = Vec::new();
+    let mut position: u8 = from;
+
+    loop {
+        position = match GRAPH[position as usize][direction as usize] {
+            Some(index) => index,
+            None => return path // <- end of board
+        };
+
+        let piece = match hexchess.board[position as usize] {
+            Some(value) => value,
+            None => {
+                path.push(position); // <- unoccupied position
+                continue;
+            }
+        };
+
+        if get_color(piece) == color {
+            return path // <- shop short of friendly piece
+        }
+        
+        path.push(position); // <- and captury enemy piece
+        return path;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::hex;
+    use super::*;
+
+    #[test]
+    fn stop_at_board_edge() {
+        assert_eq!(
+            walk(&Hexchess::new(), hex!("f6"), 0, Color::White),
+            [
+                hex!("f7"),
+                hex!("f8"),
+                hex!("f9"),
+                hex!("f10"),
+                hex!("f11"),
+            ]
+        );        
+    }
+
+    #[test]
+    fn stop_before_friendly_piece() {
+        let hexchess = Hexchess::from("1/3/2P2/7/9/5R5/11/11/11/11/11 w - 0 1").unwrap();
+
+        assert_eq!(
+            walk(&hexchess, hex!("f6"), 0, Color::White),
+            [
+                hex!("f7"),
+                hex!("f8"),
+                // f9 is a friendly pawn
+            ]
+        );        
+    }
+
+    #[test]
+    fn stop_on_hostile_piece() {
+        let hexchess = Hexchess::from("1/3/2p2/7/9/5R5/11/11/11/11/11 w - 0 1").unwrap();
+
+        assert_eq!(
+            walk(&hexchess, hex!("f6"), 0, Color::White),
+            [
+                hex!("f7"),
+                hex!("f8"),
+                hex!("f9"), // <- f9 is a hostile pawn
+            ]
+        );        
     }
 }
