d2e00511957f5e2b9991fa85a06d5cf496309dbb
Scott Bedard <scottbedard90@gmail.com>
2025-04-02 20:37:34 -0500

    knights

--- src/hexchess/pieces/pawn.rs
+++ src/hexchess/pieces/pawn.rs
@@ -7,7 +7,7 @@ use crate::constants::{
 
 pub fn pawn_moves_unsafe(
     _hexchess: &Hexchess,
-    _from: &u8,
+    _from: u8,
     _color: &Color,
 ) -> Vec<San> {
     let result: Vec<San> = vec![];
