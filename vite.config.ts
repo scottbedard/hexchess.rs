d00f352af2f0acec4e82d96500941cf3bb8f34c1
Scott Bedard <scottbedard90@gmail.com>
2025-04-05 23:12:15 -0500

    begin working on wasm integration

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
+import wasm from 'vite-plugin-wasm'
+
+export default defineConfig({
+  plugins: [
+    wasm(),
+    topLevelAwait()
+  ],
+  resolve: {
+    mainFields: ['module']
+  },
+  test: {
+    typecheck: {
+      enabled: true,
+    },
+  },
+})
