8e732f050ef2d97bd357f627a23f5e0b738a994a
Scott Bedard <scottbedard90@gmail.com>
2025-04-10 21:33:18 -0500

    adjust appended type setup

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,20 +1,32 @@
 import { read, resolve, write } from './utils.mjs'
 
 function run() {
-  const file = resolve('pkg/package.json')
-  const pkg = JSON.parse(read(file))
+  const pkg = resolve('pkg/package.json')
+  const json = JSON.parse(read(pkg))
   const base = JSON.parse(read('package.json'))
 
-  pkg.files.push('index.js')
-  pkg.main = 'index.js'
-  pkg.name = '@bedard/hexchess'
-  pkg.sideEffects.push('./index.js')
-  pkg.version = base.version
-  write(file, JSON.stringify(pkg, null, 2) + '\n')
-
-  // write module files
+  // write module
+  json.files.push('index.js')
+  json.main = 'index.js'
+  json.name = '@bedard/hexchess'
+  json.sideEffects.push('./index.js')
+  json.version = base.version
+  write(pkg, JSON.stringify(json, null, 2) + '\n')
   write('pkg/index.js', read('src/pkg.mjs').replace('x.y.z', base.version))
-  write('pkg/hexchess.d.ts', read('pkg/hexchess.d.ts').replace('x.y.z', base.version))
+
+  // append types
+  const dts = read('pkg/hexchess.d.ts')
+  const types = read('src/types.d.ts')
+    .split('\n')
+    .slice(
+      read('src/types.d.ts')
+        .split('\n')
+        .findIndex(line => line.startsWith('// @append-types-start')) + 1
+    )
+    .join('\n')
+    .replace('x.y.z', base.version)
+
+  write('pkg/hexchess.d.ts', `${dts}\n${types}`)
 }
 
 run()
