fb01add31be96ff13ee273bdd4c920361fe56ae2
Scott Bedard <scottbedard90@gmail.com>
2025-04-16 00:13:48 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import topLevelAwait from 'vite-plugin-top-level-await'
-import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
-  plugins: [
-    wasm(),
-    topLevelAwait(),
-  ],
-  resolve: {
-    mainFields: ['module'],
-  },
   test: {
-    exclude: ['src'],
+    exclude: ['node_modules', 'src'],
     typecheck: {
       enabled: true,
     },
