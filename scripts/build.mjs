a9ed4f8cfe7ae6d264c4ba8fc844c90eb57281a9
Scott Bedard <scottbedard90@gmail.com>
2025-04-16 21:36:31 -0500

    version

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,4 +1,4 @@
-import { dim, green, resolve } from './utils.mjs'
+import { dim, green, read, resolve, write } from './utils.mjs'
 import { execSync } from 'child_process'
 
 function run() {
@@ -14,9 +14,13 @@ function run() {
   execSync('rollup --config rollup.config.ts --configPlugin @rollup/plugin-typescript')
 
   // build wasm package
-  execSync('wasm-pack build ./src/wasm --out-dir ../../dist/wasm --out-name index')
+  execSync(`wasm-pack build ${resolve('src/wasm')} --out-dir ${resolve('dist/wasm')} --out-name index`)
   execSync(`rm ${resolve('dist/wasm/.gitignore')}`)
   execSync(`rm ${resolve('dist/wasm/package.json')}`)
+
+  // update version numbers
+  const pkg = JSON.parse(read('package.json'))
+  write('dist/index.mjs', read('dist/index.mjs').replace('x.y.z', pkg.version))
 
   console.log()
   console.log(`${green('Done')} ${dim(`${(Date.now() - startAt) / 1000}s`)}`)
