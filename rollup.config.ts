7e10c8c35fe31662025668c454873852655cc6cd
Scott Bedard <scottbedard90@gmail.com>
2025-04-16 21:55:38 -0500

    cleanup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'rollup'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { minify } from 'rollup-plugin-esbuild-minify'
-import { readFileSync } from 'node:fs'
 import typescript from '@rollup/plugin-typescript'
-
-const pkg = resolve(dirname(fileURLToPath(import.meta.url)), 'package.json')
-const version = JSON.parse(readFileSync(pkg, 'utf-8')).version
 
 export default defineConfig([
   {
@@ -30,18 +24,4 @@ export default defineConfig([
       typescript(),
     ],
   },
-  // {
-  //   input: 'src/cli.ts',
-  //   output: {
-  //     file: 'dist/cli.mjs',
-  //     format: 'es',
-  //   },
-  //   plugins: [
-  //     typescript(),
-  //     {
-  //       name: 'version',
-  //       transform: code => code.replace(/x\.y\.z/, version),
-  //     },
-  //   ],
-  // },
 ])
